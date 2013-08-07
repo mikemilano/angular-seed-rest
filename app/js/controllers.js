b839750e2e8582f72cb8c4286629605450c72cf5
Mike Milano <coder1@gmail.com>
2013-08-07 08:48:54 -0700

    Added more issue display functionality.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,89 @@
 'use strict';
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', 'AngularIssues', function($scope, AngularIssues) {
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', 'AngularIssues', function($scope, AngularIssues) {
     // Instantiate an object to store your scope data in (Best Practices)
-    $scope.data = {};
-    
-    AngularIssues.query(function(response) {
+    $scope.myData = {
+      currentIssue: null,
+      issueList: [],
+      issueListState: 'open',
+      issueListSort: 'created',
+      issueListDirection: 'desc',
+      issueListPage: 1
+    };
+
+    // Available default actions:
+    //  'get':    {method:'GET'},
+    //  'save':   {method:'POST'},
+    //  'query':  {method:'GET', isArray:true},
+    //  'remove': {method:'DELETE'},
+    //  'delete': {method:'DELETE'}
+    /*
+    AngularIssues.query(function(data) {
       // Assign the response INSIDE the callback
-      $scope.data.issues = response;
+      // because this is asynchonous.
+      $scope.myData.issues = data;
+      console.log('line 16');
     });
 
+    console.log('line 19');
+    */
+
+    /*
+     // Accomplish the same thing with $http
+     $http.get('https://api.github.com/repos/angular/angular.js/issues')
+     .success(function(data) {
+     console.log(data);
+     $scope.myCtrl1Data.issues = data;
+     })
+     .error(function(data) {
+     console.log(data);
+     });
+     */
+
+    $scope.setIssueList = function() {
+      AngularIssues.query({
+        state: $scope.myData.issueListState,
+        //labels: $scope.myData.labels,
+        sort: $scope.myData.issueListSort,
+        direction: $scope.myData.issueListDirection
+      }, function(data) {
+        $scope.myData.issueList = data;
+      });
+    };
+
+    $scope.setSort = function(sort) {
+      var oldSort = angular.copy($scope.myData.issueListSort);
+      $scope.myData.issueListSort = sort;
+      if (oldSort == sort) {
+        $scope.setDirection($scope.myData.issueListDirection == 'desc' ? 'asc' : 'desc');
+      } else {
+        $scope.setDirection('desc');
+      }
+    };
+
+    $scope.setDirection = function(direction) {
+      $scope.myData.issueListDirection = direction;
+      $scope.setIssueList();
+    };
+
+    $scope.sortClass = function(column){
+      return column == $scope.myData.issueListSort && 'sort-' + $scope.myData.issueListDirection;
+    };
+
+    $scope.setCurrentIssue = function(number) {
+      AngularIssues.getIssue({number: number}, function(data){
+        $scope.myData.currentIssue = data;
+      });
+    };
+
+    $scope.showAll = function() {
+      $scope.myData.currentIssue = null;
+    };
+
+    $scope.setIssueList();
   }])
-  .controller('MyCtrl2', [function() {
+
+  .controller('MyCtrl3', [function() {
 
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,6 +2,17 @@
 
 angular.module('myApp.services', ['ngResource'])
   .factory('AngularIssues', function($resource){
-    return $resource('https://api.github.com/repos/angular/angular.js/issues', {})
+    return $resource('https://api.github.com/repos/angular/angular.js/issues/:number',
+      {number: '@number'},
+      {getIssue: {method: 'GET', params: {number: 0}}}
+    )
   })
   .value('version', '0.1');
+
+/*
+{ 'get':    {method:'GET'},
+  'save':   {method:'POST'},
+  'query':  {method:'GET', isArray:true},
+  'remove': {method:'DELETE'},
+  'delete': {method:'DELETE'} };
+*/
